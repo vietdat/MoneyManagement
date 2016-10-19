487dd530ec9b84d9eff9898913c461fa56cdddb6
Dat Le <levietdat@gmail.com>
2016-10-20 03:30:44 +0700

    fix signup & add models

--- 3. Code/MoneyManagement/app/src/main/java/com/hcmut/moneymanagement/models/UserModel.java
+++ 3. Code/MoneyManagement/app/src/main/java/com/hcmut/moneymanagement/models/UserModel.java
@@ -1,6 +1,7 @@
 package com.hcmut.moneymanagement.models;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.hcmut.moneymanagement.objects.Category;
 import com.hcmut.moneymanagement.objects.Wallet;
 
 
@@ -16,6 +17,30 @@ public class UserModel extends Model {
         WalletModel walletModel = new WalletModel();
         walletModel.add(cash);
         walletModel.add(bank);
+
+        IncomeCategoryModel incomeCategoryModel = new IncomeCategoryModel();
+        incomeCategoryModel.add(new Category("Tiền lương"));
+        incomeCategoryModel.add(new Category("Tiền thưởng"));
+        incomeCategoryModel.add(new Category("Tiền lãi"));
+        incomeCategoryModel.add(new Category("Bán đồ"));
+        incomeCategoryModel.add(new Category("Khác"));
+
+        ExpenseCategoryModel expenseCategoryModel = new ExpenseCategoryModel();
+        expenseCategoryModel.add(new Category("Hóa đơn"));
+        expenseCategoryModel.add(new Category("Giao thông"));
+        expenseCategoryModel.add(new Category("Mua sắm"));
+        expenseCategoryModel.add(new Category("Giải trí"));
+        expenseCategoryModel.add(new Category("Mua sắm"));
+        expenseCategoryModel.add(new Category("Du lịch"));
+        expenseCategoryModel.add(new Category("Sức khỏe"));
+        expenseCategoryModel.add(new Category("Giáo dục"));
+        expenseCategoryModel.add(new Category("Bảo hiểm"));
+        expenseCategoryModel.add(new Category("Khác"));
+
+        WalletCategoryModel walletCategoryModel = new WalletCategoryModel();
+        walletCategoryModel.add(new Category("Tiền mặt"));
+        walletCategoryModel.add(new Category("Tài khoản ngân hàng"));
+        walletCategoryModel.add(new Category("Khác"));
     }
 
     public void write(String field, String value){
