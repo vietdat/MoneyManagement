7f8e812cacd6e0c7ced8a20ee2d0d97c28667d93
Đạt Lê <datle@goappable.com>
2016-12-24 07:04:19 +0700

    edit

---
+++ 3. Code/MoneyManagement/app/src/main/java/com/hcmut/moneymanagement/activity/FastInput/FastInputHome.java
@@ -0,0 +1,15 @@
+package com.hcmut.moneymanagement.activity.FastInput;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hcmut.moneymanagement.R;
+
+public class FastInputHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fast_input_home);
+    }
+}
--- 3. Code/MoneyManagement/app/src/main/java/com/hcmut/moneymanagement/models/TransactionModel.java
+++ 3. Code/MoneyManagement/app/src/main/java/com/hcmut/moneymanagement/models/TransactionModel.java
@@ -1,45 +1,42 @@
-//package com.hcmut.moneymanagement.models;
-//
-//
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-//import com.hcmut.moneymanagement.objects.Transaction;
-//
-//import java.lang.reflect.Field;
-//
-//public class TransactionModel extends Model{
-//
-//    public TransactionModel(){
-//        reference = FirebaseDatabase.getInstance().getReference()
-//                .child(uidEncrypted).child(encrypt("transactions"));
-//        reference.keepSynced(true);
-//    }
-//
-//    public void add(Transaction transaction){
-//        Field[] fields = Transaction.class.getFields();
-//        String key = reference.push().getKey();
-//        for (int i = 0; i < fields.length; i++){
-//            try {
-//                String fieldName = fields[i].getName();
-//                if( !fieldName.equals("serialVersionUID") && !fieldName.equals("$change")){
-//                    // Get value object of wallet
-//                    Object value = fields[i].get(transaction);
-//                    if(value != null){
-//                        String valueEncrypted = encryption.encrypt(value.toString());
-//
-//                        // Write encypted value to Firebase
-//                        reference.child(key).child(encrypt(fieldName)).setValue(valueEncrypted);
-//                    }
-//                }
-//
-//            }catch (IllegalAccessException e){
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
+package com.hcmut.moneymanagement.models;
+
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.hcmut.moneymanagement.objects.Transaction;
+
+import java.lang.reflect.Field;
+
+public class TransactionModel extends Model{
+
+    public TransactionModel(){
+        reference = FirebaseDatabase.getInstance().getReference()
+                .child(uidEncrypted).child(encrypt("transactions"));
+        reference.keepSynced(true);
+    }
+
+    public void add(Transaction transaction){
+        Field[] fields = Transaction.class.getFields();
+        String key = reference.push().getKey();
+        for (int i = 0; i < fields.length; i++){
+            try {
+                String fieldName = fields[i].getName();
+                if( !fieldName.equals("serialVersionUID") && !fieldName.equals("$change")){
+                    // Get value object of wallet
+                    Object value = fields[i].get(transaction);
+                    if(value != null){
+                        String valueEncrypted = encryption.encrypt(value.toString());
+
+                        // Write encypted value to Firebase
+                        reference.child(key).child(encrypt(fieldName)).setValue(valueEncrypted);
+                    }
+                }
+
+            }catch (IllegalAccessException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
 //    public Transaction getTransaction() {
 //        reference.addListenerForSingleValueEvent(new ValueEventListener() {
 //            @Override
@@ -52,5 +49,6 @@
 //
 //            }
 //        }
-//}
-//
+//    }
+}
+
